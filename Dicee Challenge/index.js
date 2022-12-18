ccaa3f7ee61d0d6f2e280f4f520e32542dabce22
Herby Dubic <36649042+herby989@users.noreply.github.com>
2022-12-18 06:54:07 -0800

    Dicee Challenge
    Basic game of dice roll

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,16 @@
+
+let randomNumber1 = Math.ceil(Math.random()*6);
+let randomNumber2 = Math.ceil(Math.random()*6);
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
